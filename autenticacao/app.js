318cf3218000c8b7d324cd8691e9aaba56b45215
Gabriel Souza Calixto <gabriel15806@gmail.com>
2021-08-26 21:24:11 -0300

    Correcões e inclusão do gitignore

--- autenticacao/app.js
+++ autenticacao/app.js
@@ -1,6 +1,5 @@
 const app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
-	console.log('Servidor online');
-})
+app.listen(80, () => console.log('Servidor online')
+)
